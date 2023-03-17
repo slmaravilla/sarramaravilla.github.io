d1d75e28cdd57f6aa1fab3cf7dd26d2c2ea3488f
sarramaravilla <slmaravilla@up.edu.ph>
2023-03-17 14:39:38 +0800

    updated

--- Mini Projects for ArcGIS Maps SDK for JS/Green Spaces/js/main.js
+++ Mini Projects for ArcGIS Maps SDK for JS/Green Spaces/js/main.js
@@ -6,6 +6,7 @@ require([
     "esri/layers/TileLayer",
     "esri/layers/WebTileLayer",
     "esri/layers/VectorTileLayer",
+    "esri/layers/GeoJSONLayer"
 ], function (
     Map,
     MapView,
@@ -13,7 +14,25 @@ require([
     Fullscreen,
     TileLayer,
     WebTileLayer,
-    VectorTileLayer) {
+    VectorTileLayer,
+    GeoJSONLayer) {
+
+    const layer = new GeoJSONLayer ({
+        url: "https://sarramaravilla.github.io/Mini Projects for ArcGIS Maps SDK for JS/Green Spaces/GreenSpaces.geojson"
+    });
+
+    const map = new Map ({
+        basemap: "gray-vector",
+        layers: [GeoJSONLayer]
+
+    });
+
+    const view = new MapView ({
+        container: "viewDiv",
+        center: [120.57930, 15.100],
+        zoom: 10,
+        map: map
+    });
 
 
 
