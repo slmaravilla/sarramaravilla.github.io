f1adc37db87706f801b9ffa10330f65af09e6c84
sarramaravilla <slmaravilla@up.edu.ph>
2023-03-17 17:01:53 +0800

    updated

--- Mini Projects for ArcGIS Maps SDK for JS/Green Spaces/js/main.js
+++ Mini Projects for ArcGIS Maps SDK for JS/Green Spaces/js/main.js
@@ -1,6 +1,7 @@
 require([
     "esri/Map",
     "esri/views/MapView",
+    "esri/views/SceneView",
     "esri/widgets/Home",
     "esri/widgets/Fullscreen",
     "esri/layers/TileLayer",
@@ -10,6 +11,7 @@ require([
 ], function (
     Map,
     MapView,
+    SceneView,
     Home,
     Fullscreen,
     TileLayer,
@@ -17,22 +19,43 @@ require([
     VectorTileLayer,
     GeoJSONLayer) {
 
-    const layer = new GeoJSONLayer ({
-        url: "https://sarramaravilla.github.io/Mini Projects for ArcGIS Maps SDK for JS/Green Spaces/GreenSpaces.geojson"
+    const map = new Map({
+        basemap: "dark-gray-vector",
     });
 
-    const map = new Map ({
-        basemap: "gray-vector",
-        layers: [GeoJSONLayer]
-
-    });
-
-    const view = new MapView ({
+    const view = new SceneView({
+        map: map,
         container: "viewDiv",
-        center: [120.57930, 15.100],
-        zoom: 10,
-        map: map
+        camera: {
+            position: {
+                x: 121.026551,
+                y: 14.356406,
+                z: 30000
+            },
+            tilt: 40
+        },
+        environment: {
+            background: {
+                type: "color",
+                color: [0, 0, 0, 1]
+            },
+            // disable stars
+            starsEnabled: false,
+            //disable atmosphere
+            atmosphereEnabled: false
+        }
     });
+
+    const layer = new GeoJSONLayer({
+        url: "https://sarramaravilla.github.io/Mini Projects for ArcGIS Maps SDK for JS/Green Spaces/GreenSpaces.geojson",
+        renderer :{
+            type: "simple",
+            symbol: {
+                type: "simple-marker",
+                color: "green"
+            }
+        }
+    }); map.add(layer);
 
 
 
